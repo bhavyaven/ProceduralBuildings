c360f343aba5c948536099f64834ee6eb59d4c22
Bhavya Venkat <bhavya.v04@gmail.com>
2025-04-15 10:40:39 -0400

    Add project files.

---
+++ include/ProceduralBuildings.h
@@ -0,0 +1,18 @@
+﻿// ProceduralBuildings.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include <iostream>
+#include <vector>
+
+struct Building {
+	int width, height, depth;
+	// add windows, doors, materials, etc.
+};
+
+class ProceduralBuilding {
+	public:
+		Building generateBuilding();
+};
+
+// TODO: Reference additional headers your program requires here.
---
+++ src/ProceduralBuildings.cpp
@@ -0,0 +1,13 @@
+﻿// ProceduralBuildings.cpp : Defines the entry point for the application.
+//
+
+#include "ProceduralBuildings.h"
+#include <cstdlib>
+
+Building ProceduralBuilding::generateBuilding() {
+	Building b;
+	b.width = 5;
+	b.height = 10;
+	b.depth = 5;
+	return b;
+}
